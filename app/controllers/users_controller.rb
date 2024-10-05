a3ad25f8ff52e41907246dff945e03e0ad03b8bb
philipNgungi037 <philipngungi037@gmail.com>
2024-10-05 04:13:42 +0300

    :sparkles::art: design register and login methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize_request, only: [:create, :login]
+
+    # User Registration
+    def create
+      user = User.new(user_params)
+      if user.save
+        token = JsonWebToken.encode(user_id: user.id)
+        render json: { token: token, user: user }, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    # User Login
+    def login
+      user = User.find_by(email: params[:email])
+      if user&.authenticate(params[:password])
+        token = JsonWebToken.encode(user_id: user.id)
+        render json: { token: token, user: user }, status: :ok
+      else
+        render json: { errors: 'Invalid email or password' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username, :email, :password, :password_confirmation)
+    end
 end

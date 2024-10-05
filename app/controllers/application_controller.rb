7dbdd2f884ca38708e6ff9d541c76310a29e5012
philipNgungi037 <philipngungi037@gmail.com>
2024-10-05 04:02:08 +0300

    :sparkles: Define methods to handle current user and authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    before_action :authorize_request
+
+    def current_user
+      @current_user
+    end
+  
+    private
+  
+    def authorize_request
+      header = request.headers['Authorization']
+      token = header.split(' ').last if header
+      begin
+        decoded = JsonWebToken.decode(token)
+        @current_user = User.find(decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+        render json: { errors: 'Unauthorized access' }, status: :unauthorized
+      end
+    end
 end

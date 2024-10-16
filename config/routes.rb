a1a6794bd087ac8a26cb3aa0cf4fd6211561b372
philipNgungi037 <philipngungi037@gmail.com>
2024-10-16 16:17:45 +0300

    :art: unlock all users' resources.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users, only: [:create]
+  resources :users
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+
+  delete '/logout', to: 'auth#logout'
+
 
   # Defines the root path route ("/")
   # root "posts#index"

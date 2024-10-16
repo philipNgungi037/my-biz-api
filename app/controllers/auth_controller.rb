b2b4fb2bbe94e4a7a6a168d3619f6d5f44ec451f
philipNgungi037 <philipngungi037@gmail.com>
2024-10-16 15:53:22 +0300

    :pencil2: rename my login username to email param.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,18 +3,18 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.find_by(username: login_params[:username])
+        @user = User.find_by(email: login_params[:email])
         if @user && @user.authenticate(login_params[:password])
             token = encode_token(user_id: @user.id)
             render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
         else
-            render json: { error: 'Invalid username or password' }, status: :unauthorized
+            render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
     end 
 
     private
 
     def login_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :password)
     end
 end

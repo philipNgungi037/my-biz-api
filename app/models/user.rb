8f55d46dd6d3e2f5dac761a83221115b349ac9dd
philipNgungi037 <philipngungi037@gmail.com>
2024-10-16 15:57:19 +0300

    :art: validate password in usermodel.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     validates :email, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, confirmation: true # Ensure presence and confirmation of password
 end

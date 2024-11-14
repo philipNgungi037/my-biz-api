2faed6a335838035ea6f2894efd4a67645219b96
philipNgungi037 <philipngungi037@gmail.com>
2024-11-14 14:26:19 +0300

    :art: add role to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
     validates :username, uniqueness: { case_sensitive: false }
     validates :email, uniqueness: { case_sensitive: false }
     validates :password, presence: true, confirmation: true # Ensure presence and confirmation of password
+
+     # Role validation
+    validates :role, inclusion: { in: %w[user admin], message: "%{value} is not a valid role" }
+  
+     # Set default role to 'user' if none is provided
+    after_initialize :set_default_role, if: :new_record?
+   
+    private
+   
+    def set_default_role
+       self.role ||= 'user'
+    end
 end

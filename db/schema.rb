468737b3821257660dafb08a467306ac358cecd7
philipNgungi037 <philipngungi037@gmail.com>
2024-11-14 14:22:33 +0300

    :art: add role to users table plus migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_13_102139) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_14_092535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_13_102139) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
 end

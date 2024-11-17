ef0b701620ebb1a6e50d69c2ca728aed114f8f2e
philipNgungi037 <philipngungi037@gmail.com>
2024-11-17 17:56:10 +0300

    :art: run products migration into our DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_14_113320) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_17_135445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

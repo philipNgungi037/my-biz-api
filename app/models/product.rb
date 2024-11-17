c231926c01a36970332c3a26fc3eef90bd3d4569
philipNgungi037 <philipngungi037@gmail.com>
2024-11-17 16:58:44 +0300

    :art: create all product relationships and validate its params.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+    has_many :cart_items, dependent: :destroy
+    has_many :order_items, dependent: :destroy
+  
+    validates :name, :price, :stock, presence: true
+    validates :price, numericality: { greater_than: 0 }
 end

2431463473e07d484b10f5146808a2e08aeb163a
philipNgungi037 <philipngungi037@gmail.com>
2024-11-17 17:54:32 +0300

    :art: route our products.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :users
+  resources :products
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
 

6afb53f4a36e1f4c9f1e5a00d0a5decdf632e3e0
philipNgungi037 <philipngungi037@gmail.com>
2024-11-18 15:39:32 +0300

    :art: migrating category attribute into db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_17_135445) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_094542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -22,6 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_17_135445) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_17_135445) do
     t.string "role"
   end
 
+  add_foreign_key "products", "categories"
 end

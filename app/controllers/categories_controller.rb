9aa5fa86689f735f054f1020d8b4e998efcdee9f
philipNgungi037 <philipngungi037@gmail.com>
2024-11-23 14:11:46 +0300

    :sparkles::art: add index and create methods, some of CRUDS inside categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorized, only: [:index]
+
+    def index
+        categories = Category.all
+        render json: categories
+    end
+
+    def create
+        category = Category.new(category_params)
+        if category.save
+          render json: category, status: :created
+        else
+          render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def category_params
+        params.require(:category).permit(:title, :img)
+      end
 end
